cf0cddaf60be1c74bc3877b5b9445746f1a7908a
Makarand Thatte <makthatte2010@gmail.com>
2019-09-04 19:45:29 +0530

    Web view is getting displayed, but it can't be controlled by D-PAD.

--- lib/dpad_tv_webview.dart
+++ lib/dpad_tv_webview.dart
@@ -1,24 +1,52 @@
 library dpad_tv_webview;
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewForTV extends StatelessWidget {
+class WebViewForTV extends StatefulWidget {
   final String initialUrl;
 
   const WebViewForTV({Key key, this.initialUrl}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return WebView(
+  State<StatefulWidget> createState() {
+    return _WebViewForTV();
+  }
+}
+
+class _WebViewForTV extends State<WebViewForTV> {
+  WebViewController _controller;
+  WebView _webView;
+  Set<Factory<OneSequenceGestureRecognizer>> _gestureRecognizers;
+  String _userAgent;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _webView = WebView(
       onWebViewCreated: _handleWebViewWidget,
-      initialUrl: initialUrl,
+      initialUrl: widget.initialUrl,
       javascriptMode: JavascriptMode.unrestricted,
+      navigationDelegate: _navigationDelegate,
+      gestureRecognizers: _gestureRecognizers,
       onPageFinished: _handlePageFinished,
+      userAgent: _userAgent,
     );
   }
 
-  void _handleWebViewWidget(WebViewController controller) {}
+  @override
+  Widget build(BuildContext context) {
+    return _webView;
+  }
+
+  void _handleWebViewWidget(WebViewController controller) {
+    _controller = controller;
+  }
 
   void _handlePageFinished(String url) {}
+
+  NavigationDecision _navigationDelegate(NavigationRequest navigation) {}
 }
